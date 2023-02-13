ac7ba15756a532eacf4dcdfb0e98b46d2fd9f984
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-13 17:48:00 +0300

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,15 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[0];
+        for (int i = 0; i < nums.length - 1; i++) {
+          for (int j = i+1; j< nums.length;j++) {
+            if (nums[i] + nums[j] == target) {
+                result = new int[]{i, j};
+            }
+          }
+        }
+        return result;
+    }
+}
