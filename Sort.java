fcb1bc29dba59ecfba1f7a0dd6fdfadfd0499125
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-13 15:45:45 +0300

    Create Sort.java

---
+++ Sort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Sort  {
+    public static void main(String[] args) {
+        String str =  "Strings are constant; their values cannot be changed after they are created."
+                .replace(";"," ")
+                .replace("."," ")
+                .toLowerCase();
+        String[] str1 = str.split(" ");
+        sortWords(str1);
+
+
+    }
+
+    public static String sortWords(String[] str1) {
+        Arrays.sort(str1);
+        for (int i = 0; i < str1.length; i++)
+           System.out.print(str1[i] + " ");
+
+        System.out.println();
+
+        Arrays.stream(str1)
+                .sorted((i1,i2) -> Integer.compare(i1.length(),i2.length()))
+                .forEach(System.out::println);
+
+
+        return String.join(" ",str1);
+            }
+        }
+
+
+
+
+
+
