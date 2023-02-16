34500038b45209b309d42a647b5305151811779b
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-16 13:39:08 +0300

    Update Prefix.java

--- Prefix.java
+++ Prefix.java
@@ -1,3 +1,6 @@
+//a function to find the longest common prefix string amongst an array of strings.
+//If there is no common prefix, return an empty string "".
+
 public class Prefix {
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0)
