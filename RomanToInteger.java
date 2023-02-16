3583e364598868d8d21324bff96ee412bb926c59
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-16 13:37:59 +0300

    Update RomanToInteger.java

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,3 +1,6 @@
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+//Given a roman numeral, convert it to an integer.
+
 import java.util.HashMap;
 import java.util.Map;
 
