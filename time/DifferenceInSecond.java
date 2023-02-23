cfc4fd67de61451930c0849a255b5f0f4a4b03cf
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-23 20:20:40 +0300

    Create DifferenceInSecond.java

---
+++ time/DifferenceInSecond.java
@@ -0,0 +1,12 @@
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class DifferenceInSecond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LocalTime time1 = LocalTime.parse(sc.next());
+        LocalTime time2 = LocalTime.parse(sc.next());
+        int time = time1.toSecondOfDay() - time2.toSecondOfDay();
+        System.out.println(Math.abs(time));
+    }
+}
