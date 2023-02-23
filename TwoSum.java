528b64166138e5613b4b6c6c6d18069ed6388b55
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-23 19:57:27 +0300

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,4 +1,7 @@
-//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/*
+    Given an array of integers nums and an integer target, 
+    return indices of the two numbers such that they add up to target.
+*/
 
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
