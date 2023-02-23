6d91316f3bb2e2556b960f1447fe10ef35696113
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-23 19:56:43 +0300

    Create Sqrttt.java

---
+++ Sqrttt.java
@@ -0,0 +1,18 @@
+/*
+    Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+    The returned integer should be non-negative as well.
+
+    You must not use any built-in exponent function or operator.
+*/
+
+class Sqrttt {
+    public int mySqrt(int x) {
+       if (x < 2) return x;
+        int i=1;
+        while (i*i<=x){
+            if(i*i<=0) break;
+            i++;
+        }
+        return --i;
+    }
+}
