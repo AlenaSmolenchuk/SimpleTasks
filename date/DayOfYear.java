2843805392ad7ccb36bce540c90caccd166b82d8
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-23 20:25:26 +0300

    Update DayOfYear.java

--- date/DayOfYear.java
+++ date/DayOfYear.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 import java.util.Scanner;
 
-public class Main {
+public class DayOfYear {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
