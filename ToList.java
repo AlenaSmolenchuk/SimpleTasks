329f5578676ad0b3a2579eeeada98b141d8a7b89
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-25 18:31:10 +0300

    Update ToList.java

--- ToList.java
+++ ToList.java
@@ -1,6 +1,8 @@
-//The array-form of an integer num is an array representing its digits in left to right order.
-//For example, for num = 1321, the array form is [1,3,2,1].
-//Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+/*
+    The array-form of an integer num is an array representing its digits in left to right order.
+    For example, for num = 1321, the array form is [1,3,2,1].
+    Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+*/
 
 import java.util.ArrayList;
 import java.util.Collections;
