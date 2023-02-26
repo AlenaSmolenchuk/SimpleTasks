30c261f970e870892f7429151cb08d80c3c8e2f5
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-02-26 17:14:09 +0300

    Create PalindromeNodeList.java

---
+++ palindrome/PalindromeNodeList.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListNode {
+     int val;
+     static ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public static void main(String[] args) {
+        isPalindrome(next);
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<>();
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
