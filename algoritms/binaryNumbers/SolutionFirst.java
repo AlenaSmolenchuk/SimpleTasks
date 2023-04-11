d856ecb00210075120c4dff91ac296e610e481db
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-04-11 15:58:31 +0300

    Update SolutionFirst.java

--- algoritms/binaryNumbers/SolutionFirst.java
+++ algoritms/binaryNumbers/SolutionFirst.java
@@ -5,7 +5,7 @@ class SolutionFirst {
     public String addBinary(String a, String b) {
         BigInteger A = new BigInteger(a,2);
         BigInteger B = new BigInteger(b,2); 
-        BigInteger add = A.add(B);
+        BigInteger add = A.add(B); 
          
         return add.toString(2);
     }
