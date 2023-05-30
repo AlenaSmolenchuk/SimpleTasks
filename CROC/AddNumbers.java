ff2df3139d9ad63f1468b2575a008c6869997c95
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-05-30 15:52:31 +0300

    Create AddNumbers.java

---
+++ CROC/AddNumbers.java
@@ -0,0 +1,27 @@
+package CROC;
+
+import java.util.Scanner;
+
+public class TestTask {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            int n = scanner.nextInt();
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                sum += arr[i];
+            }
+
+            System.out.println(sum);
+
+            scanner.close();
+        }
+}
