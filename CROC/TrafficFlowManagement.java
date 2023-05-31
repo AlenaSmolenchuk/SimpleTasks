63ab7768ceb3d48818fffbeaefea53d6f18c59c1
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-05-31 15:18:33 +0300

    Update TrafficFlowManagement.java

--- CROC/TrafficFlowManagement.java
+++ CROC/TrafficFlowManagement.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 public class Task3 {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+       
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         int d = scanner.nextInt();
