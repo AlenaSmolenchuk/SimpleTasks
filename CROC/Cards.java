df8ab6e3be65f841fc6d7a4d53c944198d9b9a98
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-06-03 14:27:13 +0300

    Update Cards.java

--- CROC/Cards.java
+++ CROC/Cards.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
         int n = scanner.nextInt();
 
         if (n < 1  n > 100) {
