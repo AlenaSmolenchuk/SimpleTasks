a253eb77ee544e7d1a978289e96a44cf534dfd0b
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-06-09 22:50:05 +0300

    Update AddNumbers.java

--- CROC/AddNumbers.java
+++ CROC/AddNumbers.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class TestTask {
         public static void main(String[] args) {
+                
             Scanner scanner = new Scanner(System.in);
 
             int n = scanner.nextInt();
