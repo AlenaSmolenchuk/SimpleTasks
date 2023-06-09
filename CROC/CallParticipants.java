a97d8de48deb003c368903e402ef1eefe3787b6c
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-06-09 22:50:17 +0300

    Update CallParticipants.java

--- CROC/CallParticipants.java
+++ CROC/CallParticipants.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Task {
         public static void main(String[] args) {
+                
             Scanner scanner = new Scanner(System.in);
             int n = scanner.nextInt();
             int[] calls = new int[n];
