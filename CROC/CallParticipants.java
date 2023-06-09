650a1419eba2d77816c9a3734d655c094a074d9b
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-06-09 22:50:37 +0300

    Update CallParticipants.java

--- CROC/CallParticipants.java
+++ CROC/CallParticipants.java
@@ -8,13 +8,16 @@ public class Task {
         public static void main(String[] args) {
                 
             Scanner scanner = new Scanner(System.in);
+                
             int n = scanner.nextInt();
             int[] calls = new int[n];
+                
             for (int i = 0; i < n; i++) {
                 calls[i] = scanner.nextInt();
             }
 
             Map<Integer, Integer> callParticipants = new HashMap<>();
+                
             for (int i = 0; i < n; i++) {
                 int callId = calls[i];
                 if (callParticipants.containsKey(callId)) {
