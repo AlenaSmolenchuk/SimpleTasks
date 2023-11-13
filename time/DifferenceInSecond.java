adce4c4ec5ad54398fb97974f00bf2c6044f157b
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-11-13 15:54:55 +0300

    Update DifferenceInSecond.java

--- time/DifferenceInSecond.java
+++ time/DifferenceInSecond.java
@@ -5,9 +5,12 @@ public class DifferenceInSecond {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
+        
         LocalTime time1 = LocalTime.parse(sc.next());
         LocalTime time2 = LocalTime.parse(sc.next());
+        
         int time = time1.toSecondOfDay() - time2.toSecondOfDay();
+        
         System.out.println(Math.abs(time));
     }
 }
