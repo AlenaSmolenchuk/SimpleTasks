bea7948f6471e2c5a74d98e9b2aae0d0fbcefe17
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-12-18 20:58:17 +0300

    Update DayOfYear.java

--- date/DayOfYear.java
+++ date/DayOfYear.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 
 public class DayOfYear {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
 
         int year = Integer.parseInt(sc.next());
         int day1 = Integer.parseInt(sc.next());
         int day2 = Integer.parseInt(sc.next());
         int day3 = Integer.parseInt(sc.next());
+        
         LocalDate day1inyear = LocalDate.ofYearDay(year, day1);
         LocalDate day2inyear = LocalDate.ofYearDay(year, day2);
         LocalDate day3inyear = LocalDate.ofYearDay(year, day3);
+        
         System.out.println(day1inyear);
         System.out.println(day2inyear);
         System.out.println(day3inyear);
