9769bcb74925227ec4bf412fc74170a89315a344
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2024-01-25 11:53:54 +0300

    Task2.java aktualisieren

--- yandex/Task2.java
+++ yandex/Task2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Task2 {
 
     public static void main(String[] args) {
-
+ 
         Scanner sc = new Scanner(System.in);
         int countLetters = sc.nextInt();
 
